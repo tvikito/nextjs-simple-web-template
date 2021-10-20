0cd4f68498f8f94caec27fcb1311c124a795a13b
Tomas Vykoukal <vykoukal.tom@gmail.com>
2021-10-20 16:53:51 +0200

    Adjust Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,25 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-const colors = require('tailwindcss/colors')
+// const colors = require('tailwindcss/colors')
 
 module.exports = {
   mode: 'jit',
   purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      ...colors,
-      black: { light: '#141414', DEFAULT: colors.black },
-      winder: '#ff6600',
-      v4: '#ea1c13',
-      cyber: '#67df31',
-    },
+    // Rewrite default colors
+    // colors: {
+    //   ...colors,
+    //   black: { ...colors.black, light: '#141414', DEFAULT: colors.black },
+    // },
     extend: {
-      screens: {
-        xs: '340px',
-        ...defaultTheme.screens,
-      },
+      // Add custom colors
+      // colors: {
+      //   black: { light: '#141414', DEFAULT: colors.black },
+      //   winder: '#ff6600',
+      // },
+      // Add custom screen widths
+      //   screens: {
+      //     xs: '340px',
+      //   },
     },
   },
   variants: {
