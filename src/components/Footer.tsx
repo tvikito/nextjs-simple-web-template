818bc5a07bedca9675e7220169ffc3306cf67e01
Tomas Vykoukal <vykoukal.tom@gmail.com>
2022-12-11 17:44:24 +0100

    footer cleanup

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,36 +11,12 @@ const Footer: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
     <footer className="">
       <div className="px-5">
         {children}
-        <div>
-          <div className=" w-full text-center">
-            <h3>Subscribe for recent innovations!</h3>
-            <form method="post" noValidate onSubmit={handleSubmitForm}>
-              <div className=" xs:flex rounded-lg overflow-hidden max-w-screen-sm mx-auto">
-                <input
-                  type="email"
-                  value={email}
-                  className="email text-black w-full  sm:flex-1"
-                  placeholder="email address"
-                  required
-                  onChange={({ target: { value } }) => setEmail(value)}
-                />
-                <button
-                  type="submit"
-                  name="subscribe"
-                  className="w-full xs:w-auto"
-                >
-                  Subscribe
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
 
         <div className="pb-5 text-gray-400 flex flex-col items-center">
           Made by{' '}
           <a
             href="http://tomasvykoukal.com/"
-            className="font-bold hover:underline no-arrow text-sm"
+            className="font-bold hover:underline text-sm"
             target="_blank"
             rel="noreferrer"
           >
